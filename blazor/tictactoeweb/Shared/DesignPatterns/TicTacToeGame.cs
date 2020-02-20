a484a6f0d3b94099dc297310ea0ed4239f92a655
Iyalla Alamina (Researcher) <U1273400@pgr.hud.ac.uk>
2020-02-20 14:14:54 +0000

    f01

--- blazor/tictactoeweb/Shared/DesignPatterns/TicTacToeGame.cs
+++ blazor/tictactoeweb/Shared/DesignPatterns/TicTacToeGame.cs
@@ -1,25 +1,27 @@
 
 using System;
 using System.Threading.Tasks;
-using Microsoft.JSInterop;
-using Microsoft.AspNetCore.Blazor.Components;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
 
 namespace tictactoeweb.Shared.DesignPatterns
 {
     public class TicTacToeGame
     {
-        private final _random = new Random();
-        public int[][] winstates = new int {
-            {0, 1, 2},
-            {3, 4, 5},
-            {6, 7, 8},
-            {0, 3, 6},
-            {1, 4, 7},
-            {2, 5, 8},
-            {0, 4, 8},
-            {2, 4, 6},
+        private Random _random = new Random();
+        public int[][] winstates = new int[][] {
+            new int[]{0, 1, 2},
+            new int[]{3, 4, 5},
+            new int[]{6, 7, 8},
+            new int[]{0, 3, 6},
+            new int[]{1, 4, 7},
+            new int[]{2, 5, 8},
+            new int[]{0, 4, 8},
+            new int[]{2, 4, 6},
         };
-        public String[] gameState = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+        public String[] gameState = {"-","-","-","-","-","-","-","-","-"};
         private bool XTurnToPlay = true;
         public String winner="";
         public int windex=-1;
@@ -32,13 +34,13 @@ namespace tictactoeweb.Shared.DesignPatterns
             if (tictactoe.isWinState())break;
             }
             if(tictactoe.isWinState())
-            print((tictactoe.XTurnToPlay ? 'O' : 'X') + ' wins');
+            	Console.WriteLine((tictactoe.XTurnToPlay ? "O" : "X") + " wins");
             else 
-            print('game was a draw');
+            	Console.WriteLine("game was a draw");
             tictactoe.printState();
         }
         public void reset(){
-            this.gameState = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+            this.gameState = new String[] {"-","-","-","-","-","-","-","-","-"};
             this.XTurnToPlay = true;
             this.winner="";
             this.windex=-1;
@@ -49,20 +51,20 @@ namespace tictactoeweb.Shared.DesignPatterns
         }
         public void getNextState() {
             int v = _next(0, 9);
-            while (this.gameState[v] != '-') {
+            while (this.gameState[v] != "-") {
             v = _next(0, 9);
             }
-            this.gameState[v] = this.XTurnToPlay ? 'X' : 'O';
+            this.gameState[v] = this.XTurnToPlay ? "X" : "O";
             this.XTurnToPlay = !this.XTurnToPlay;
-            this.winner=this.isWinState()?(this.XTurnToPlay?'O':'X')+' wins':(this.isBoardFilled()?'game was a draw':'');
+            this.winner=this.isWinState()?(this.XTurnToPlay?"O":"X")+" wins":(this.isBoardFilled()?"game was a draw":"");
             //print('this.windex=${this.windex}');
             // this.testWinState();
         }
 
         public bool isWinState() {
             var winstate=false;
-            for(var i=0;i<this.winstates.length;i++){
-            if(gameState[winstates[i][0]]!='-' && gameState[winstates[i][0]]==gameState[winstates[i][1]] && gameState[winstates[i][1]]==gameState[winstates[i][2]]){
+            for(var i=0;i<this.winstates.Length;i++){
+            if(gameState[winstates[i][0]]!="-" && gameState[winstates[i][0]]==gameState[winstates[i][1]] && gameState[winstates[i][1]]==gameState[winstates[i][2]]){
                 this.windex=i;
                 winstate=true;
                 break;
@@ -72,16 +74,16 @@ namespace tictactoeweb.Shared.DesignPatterns
         }
 
         public bool isBoardFilled() {
-            return !this.gameState.contains('-');
+            return !this.gameState.ToList().Contains("-");
         }
         
         public void printState(){
-            var sb=StringBuffer();
+            var sb=new StringBuilder();
             for(var i=0;i<3;i++){
-            for(var j=0;j<3;j++)
-                sb.write(this.gameState[i*3+j]);
-            print(sb);
-            sb.clear();
+				for(var j=0;j<3;j++)
+					sb.Append(this.gameState[i*3+j]);
+				Console.WriteLine(sb.ToString());
+            sb.Clear();
             }
         }
 
@@ -89,12 +91,13 @@ namespace tictactoeweb.Shared.DesignPatterns
         * Generates a positive random integer uniformly distributed on the      range
         * from [min], inclusive, to [max], exclusive.
         */
-        private int _next(int min, int max) => min + _random.nextInt(max - min);
+        private int _next(int min, int max) => min + _random.Next(max - min);
+		
         public void testWinState(){
-            for(var i=0;i<this.winstates.length;i++){
-            print("${this.winstates[i][0]}=${this.gameState[this.winstates[i][0]]}:${this.gameState[this.winstates[i][0]]!='-'}");
-            print("${this.winstates[i][1]}=${this.gameState[this.winstates[i][1]]}:${this.gameState[this.winstates[i][1]]!='-'}");
-            print("${this.winstates[i][2]}=${this.gameState[this.winstates[i][2]]}:${this.gameState[this.winstates[i][2]]!='-'}");
+            for(var i=0;i<this.winstates.Length;i++){
+				//Console.WriteLine(@"${this.winstates[i][0]}=${this.gameState[this.winstates[i][0]]}:${this.gameState[this.winstates[i][0]]!="-"}");
+				//Console.WriteLine(@"${this.winstates[i][1]}=${this.gameState[this.winstates[i][1]]}:${this.gameState[this.winstates[i][1]]!="-"}");
+				//Console.WriteLine(@"${this.winstates[i][2]}=${this.gameState[this.winstates[i][2]]}:${this.gameState[this.winstates[i][2]]!="-"}");
             }
         }
     }
