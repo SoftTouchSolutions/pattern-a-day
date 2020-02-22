105a73eb49621980691b91b171f2cef64bc8adb4
John Alamina <john.alamina@netsells.co.uk>
2020-02-22 04:34:19 +0000

    f04

--- blazor/tictactoeweb/Shared/Helpers/HttpClientJson2.cs
+++ blazor/tictactoeweb/Shared/Helpers/HttpClientJson2.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using tictactoeweb.Shared.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace tictactoeweb.Shared.Helpers
 {
